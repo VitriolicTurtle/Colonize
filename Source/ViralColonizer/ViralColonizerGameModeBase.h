b673764593432e3dbd48c7ceeb2af3b9953279aa
Vegardoa <voaarnes@gmail.com>
2020-10-28 19:59:34 +0100

    Added initial files

---
+++ Source/ViralColonizer/ViralColonizerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ViralColonizerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRALCOLONIZER_API AViralColonizerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
